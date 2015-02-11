b5b09f9e934df2872551351e8426d79fa90d2b30
@ProfNandaa <profnandaa@gmail.com>
2015-02-11 18:01:46 +0300

    First class, into python

---
+++ hello.py
@@ -0,0 +1,21 @@
+print "Hello Word!"
+
+#Python is Dynamically
+#typed
+x = "30"
+print x * 3
+
+x = 30
+print x * 3
+
+#sample function
+
+def my_sum(a,b):
+	return a + b
+
+y = input("Value of A: ")
+z = input("Value of B: ")
+
+total = my_sum(y,z)
+
+print "A + B = ",total
